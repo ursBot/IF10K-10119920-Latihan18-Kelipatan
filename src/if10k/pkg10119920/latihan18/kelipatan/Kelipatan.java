3ea955c895c35dc6c3850a6a039495dc2c4bab6e
ursBot <umar.10119920@mahasiswa.ac.id>
2020-10-29 21:56:54 +0700

    upload pertama

---
+++ src/if10k/pkg10119920/latihan18/kelipatan/Kelipatan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119920.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Umar Said Adi Pranoto
+ * KELAS    : IF10K
+ * NIM      : 10119920
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+
+public class Kelipatan {
+
+    public static void main(String[] args) {
+        
+        for (double i = 1; i <= 10; i++){
+            System.out.println(i*3.5);
+        }
+    }
+    
+}
